5bd1b044af8823285ec4f1d8eaa2d0aa421b0c4f
AgungDita <ditawida123@gmail.com>
2025-10-02 14:59:36 +0800

    script.js

--- script.js
+++ script.js
@@ -67,7 +67,7 @@ ScrollReveal().reveal(".home-content p, .about-content", { origin: "right" });
     Typed Js 
 ====================== */
 const typed = new Typed(".multiple-text", {
-  strings: ["Informatics Student", "Web Developer"],
+  strings: ["Informatics Student"],
   typeSpeed: 100,
   backSpeed: 100,
   backDelay: 1000,
